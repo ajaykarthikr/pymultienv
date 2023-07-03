9944e5dde41c623ed36059c1967e06cbabedf70f
Ajay Karthik <ajaykarthik.r23@gmail.com>
2023-07-03 18:08:31 +0530

    refactor:  GetEnv -> load_env Convert module to singleton with load_env method instead of class.

---
+++ newenvreader.py
@@ -0,0 +1,99 @@
+"""
+This module provides utility functions for reading environment variables.
+"""
+
+import os
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def parse_env_file(path: str) -> dict[str, str]:
+    """Parse a .env file.
+
+    :param path: Path to the .env file.
+    :type path: str
+    :return: A dictionary of environment variables.
+    :rtype: dict
+    """
+    env = {}
+    with open(path, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            key, value = line.split("=", maxsplit=1)
+            env[key] = value
+    return env
+
+
+def search_env_file(start_path: str) -> str:
+    """Search for a .env file in the current directory and its parent directories.
+
+    :param start_path: Start directory to search for the .env file.
+    :type start_path: str
+    :raises FileNotFoundError: If no .env file is found.
+    :return: The path to the .env file.
+    :rtype: str
+
+    """
+    current_dir = os.path.abspath(start_path)
+
+    for root, _, files in os.walk(current_dir):
+        for file in files:
+            if file.endswith(".env"):
+                return os.path.join(root, file)
+
+    parent_dir = os.path.dirname(current_dir)
+    if parent_dir == current_dir:
+        # Reached the root directory, file not found
+        raise FileNotFoundError("No .env file found")
+
+    return search_env_file(parent_dir)
+
+
+def load_env() -> dict[str, str]:
+    """Load environment variables from the .env file or the system environment.
+
+    :raises KeyError: If a required environment variable is not found.
+    :return: A dictionary of environment variables.
+    :rtype: dict[str, str]
+    """
+    env = {}
+    for key, value in os.environ.items():
+        env[key] = value
+
+    try:
+        found_env_path = search_env_file(os.getcwd())
+        env.update(parse_env_file(found_env_path))
+    except FileNotFoundError:
+        pass
+
+    return env
+
+
+loaded_env = load_env()
+
+
+def get_env(key: str, required: bool = True, cast: type[T] = str) -> T:
+    """Load environment variable from the .env file or the system environment.
+
+    :param key: The environment variable key.
+    :type key: str
+    :param required: Whether the environment variable is required, defaults to True
+    :type required: bool, optional
+    :param cast: The type to cast the environment variable to, defaults to str
+    :type cast: type, optional
+    :raises KeyError: If the environment variable is not found and is required.
+    :return: The environment variable value.
+    :rtype: str
+    """
+    try:
+        val = loaded_env[key]
+        return cast(val)
+    except KeyError as err:
+        if required:
+            raise KeyError(f"Environment variable {key} is not found") from err
+        val = ""
+
+    return cast(val)
--- tests/test_mod.py
+++ tests/test_mod.py
@@ -1,10 +1,10 @@
-import unittest
-from unittest.mock import patch
+import importlib
 import os
 import tempfile
+import unittest
+from unittest.mock import patch
 
-from newenvreader import GetEnv
-
+import newenvreader
 
 class TestReadEnv(unittest.TestCase):
     def setUp(self):
@@ -14,22 +14,28 @@ class TestReadEnv(unittest.TestCase):
         self.temp_dir = tempfile.TemporaryDirectory()
 
     def test_read_env(self):
+
         file_path = os.path.join(self.temp_dir.name, ".env")
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(self.env_data)
 
-        # self.assertEqual(s, "my_value")
         with patch("os.getcwd", return_value=self.temp_dir.name):
-            val = GetEnv("MY_VAR").to_float()
+            importlib.reload(newenvreader)
+            
+            val = newenvreader.get_env("MY_VAR", cast=int)
             assert val == 12121.0
 
-            val = GetEnv("VAR1").to_str()
+            val = newenvreader.get_env("VAR1")
+            assert val == "value1"
+
+            val = newenvreader.get_env("VAR1")
             assert val == "value1"
 
         # Delete the temp file
         os.remove(file_path)
 
     def test_root_env_file(self):
+
         subdir_path = os.path.join(self.temp_dir.name, "subdir")
         os.mkdir(subdir_path)
 
@@ -38,13 +44,15 @@ class TestReadEnv(unittest.TestCase):
             file.write(self.env_data)
 
         with patch("os.getcwd", return_value=subdir_path):
-            val = GetEnv("MY_VAR").to_float()
+            importlib.reload(newenvreader)
+            
+            val = newenvreader.get_env("MY_VAR", cast=float)
             assert val == 12121.0
 
-            val = GetEnv("VAR1").to_str()
+            val = newenvreader.get_env("VAR1")
             assert val == "value1"
 
-            val = GetEnv("VAR3").to_int()
+            val = newenvreader.get_env("VAR3", cast=int)
             assert val == 12121
 
         # Delete the temp file
